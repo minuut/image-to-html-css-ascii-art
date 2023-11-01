35442913734e1864c2b75abad291676e4b91f884
Yonnie <70378641+minuut@users.noreply.github.com>
2023-11-01 17:39:45 +0100

    Update ImageToAscii.php

--- php/ImageToAscii.php
+++ php/ImageToAscii.php
@@ -1,128 +1,132 @@
 <?php
 
 /**
- * Class ImageToAscii
- * Converts images to HTML/CSS ASCII art.
+ * Class to convert images to ASCII.
  */
 class ImageToAscii
 {
-    private $img;
-    private $width;
-    private $height;
+  private $img;
+  private $width;
+  private $height;
 
-    /**
-     * ImageToAscii constructor.
-     * @param string $imagePath
-     * @throws Exception
-     */
-    public function __construct(string $imagePath)
-    {
-        $this->setImage($imagePath);
-        $this->width = imagesx($this->img);
-        $this->height = imagesy($this->img);
+  /**
+   * ImageToAscii constructor.
+   * @param string $imagePath - Path of the image to be converted.
+   * @throws \Exception
+   */
+  public function __construct(string $imagePath)
+  {
+    $type = exif_imagetype($imagePath);
+    switch ($type) {
+      case IMAGETYPE_PNG:
+        $this->img = imagecreatefrompng($imagePath);
+        break;
+      case IMAGETYPE_JPEG:
+        $this->img = imagecreatefromjpeg($imagePath);
+        break;
+      default:
+        throw new \Exception('Invalid image type');
+    }
+    $this->width = imagesx($this->img);
+    $this->height = imagesy($this->img);
+  }
+
+  /**
+   * Converts the image to ASCII.
+   * @param int $newWidth - Width for the converted image.
+   * @param string $characters - Characters for the ASCII representation.
+   * @throws \Exception
+   */
+  protected function convertImage(int $newWidth, string $characters)
+  {
+    $aspectRatio = $this->height / $this->width;
+    $newHeight = $newWidth * $aspectRatio;
+    $img = imagecreatetruecolor($newWidth, $newHeight);
+
+    if (!$img) {
+      throw new \Exception('Error creating image');
     }
 
-    /**
-     * Sets the image resource.
-     * @param string $imagePath
-     * @throws Exception
-     */
-    private function setImage(string $imagePath): void
-    {
-        $type = exif_imagetype($imagePath);
-        switch ($type) {
-            case IMAGETYPE_PNG:
-                $this->img = imagecreatefrompng($imagePath);
-                break;
-            case IMAGETYPE_JPEG:
-                $this->img = imagecreatefromjpeg($imagePath);
-                break;
-            default:
-                throw new \Exception('Invalid image type');
+    imagecopyresampled($img, $this->img, 0, 0, 0, 0, $newWidth, $newHeight, $this->width, $this->height);
+
+
+    imagefilter($img, IMG_FILTER_CONTRAST, 10);
+    $width = imagesx($img);
+    $height = imagesy($img);
+    for ($h = 0; $h < $height; $h++) {
+      for ($w = 0; $w < $width; $w++) {
+        // Get color at pixel location.
+        $rgb = ImageColorAt($img, $w, $h);
+        // Convert color into usable format.
+        $r = ($rgb >> 16) & 0xFF;
+        $g = ($rgb >> 8) & 0xFF;
+        $b = $rgb & 0xFF;
+        // Convert RGB to Hex
+        $hex = "#" . str_pad(dechex($r), 2, "0", STR_PAD_LEFT) .  str_pad(dechex($g), 2, "0", STR_PAD_LEFT) . str_pad(dechex($b), 2, "0", STR_PAD_LEFT);
+        // Check for white/off-white color.
+        if (($r > 240 && $g > 240 && $b > 240)) {
+          echo '&nbsp;';
+        } else {
+          echo '<span style="color:' . $hex . ';">' . $characters[rand(0, strlen($characters) - 1)] . '</span>';
         }
+      }
+      echo "\n";
     }
+  }
 
-    /**
-     * Resamples the image.
-     * @param int $newWidth
-     * @return resource
-     * @throws Exception
-     */
-    private function getResampledImage(int $newWidth)
-    {
-        $aspectRatio = $this->height / $this->width;
-        $newHeight = $newWidth * $aspectRatio;
-        $img = imagecreatetruecolor($newWidth, $newHeight);
-        if (!$img) {
-            throw new \Exception('Error creating image');
-        }
-        imagecopyresampled($img, $this->img, 0, 0, 0, 0, $newWidth, $newHeight, $this->width, $this->height);
-        imagefilter($img, IMG_FILTER_CONTRAST, 10);
-        return $img;
+  /**
+   * Displays the starting div and pre tags for ASCII output.
+   * @param int $fontSize - Font size for the display.
+   * @param string $className - CSS class name for the wrapper.
+   */
+  protected function displayImage(int $fontSize, string $className)
+  {
+    echo '<div class="img-wrapper' . $className . '" style="display: flex; justify-content: center; align-items: center; background-color: #000000;">';
+    echo '<pre style="font-size: ' . $fontSize . 'px;font-weight: bold;padding: 0px 0px;';
+
+    if ($className === "8px") {
+      echo 'line-height: 7px; letter-spacing: 3px;';
+    } else {
+      echo 'line-height: 5px; letter-spacing: 2.75px;';
     }
+    echo '">';
+  }
 
-    /**
-     * Displays the image container.
-     * @param int $fontSize
-     * @param string $className
-     */
-    private function displayImage(int $fontSize, string $className = ""): void
-    {
-        $style = "font-size: {$fontSize}px; font-weight: bold; padding: 0;";
-        $style .= $className === "8px" ? 'line-height: 7px; letter-spacing: 3px;' : 'line-height: 5px; letter-spacing: 2.75px;';
-        echo '<div class="img-wrapper' . $className . '" style="display: flex; justify-content: center; align-items: center; background-color: #000000;">';
-        echo '<pre style="' . $style . '">';
-    }
+  /**
+   * Closes the ASCII display.
+   */
+  protected function closeImage()
+  {
+    echo '</pre>';
+    echo '</div>';
+  }
 
-    /**
-     * Closes the image container.
-     */
-    private function closeImage(): void
-    {
-        echo '</pre>';
-        echo '</div>';
-    }
+  public function convert4pxSingleCharacter(string $characters = "y", int $fontSize = 4, string $className = "")
+  {
+    $this->displayImage($fontSize, $className);
+    $this->convertImage(150, $characters);
+    $this->closeImage();
+  }
 
-    /**
-     * Converts the image to ASCII art.
-     * @param int $newWidth
-     * @param string $characters
-     */
-    private function convertImage(int $newWidth, string $characters): void
-    {
-        $img = $this->getResampledImage($newWidth);
-        $width = imagesx($img);
-        $height = imagesy($img);
+  public function convert8pxSingleCharacter(string $characters = "y", int $fontSize = 8, string $className = "8px")
+  {
+    $this->displayImage($fontSize, $className);
+    $this->convertImage(100, $characters);
+    $this->closeImage();
+  }
 
-        for ($h = 0; $h < $height; $h++) {
-            for ($w = 0; $w < $width; $w++) {
-                $rgb = ImageColorAt($img, $w, $h);
-                $r = ($rgb >> 16) & 0xFF;
-                $g = ($rgb >> 8) & 0xFF;
-                $b = $rgb & 0xFF;
-                
-                if ($r > 240 && $g > 240 && $b > 240) {
-                    echo '&nbsp;';
-                } else {
-                    $hex = sprintf("#%02X%02X%02X", $r, $g, $b);
-                    echo '<span style="color:' . $hex . ';">' . $characters[rand(0, strlen($characters) - 1)] . '</span>';
-                }
-            }
-            echo "\n";
-        }
-    }
+  public function convert4pxWithMultipleCharacters(string $characters = "musti", int $fontSize = 4, string $className = "")
+  {
+    $this->displayImage($fontSize, $className);
+    $this->convertImage(150, $characters);
+    $this->closeImage();
+  }
 
-    /**
-     * Converts the image to ASCII art based on parameters.
-     * @param string $characters
-     * @param int $fontSize
-     * @param int $newWidth
-     * @param string $className
-     */
-    public function convertToAscii(string $characters, int $fontSize, int $newWidth, string $className = ""): void
-    {
-        $this->displayImage($fontSize, $className);
-        $this->convertImage($newWidth, $characters);
-        $this->closeImage();
-    }
+  public function convert8pxWithMultipleCharacters(string $characters = "musti", int $fontSize = 8, string $className = "8px")
+  {
+    $this->displayImage($fontSize, $className);
+    $this->convertImage(100, $characters);
+    $this->closeImage();
+  }
 }
+
