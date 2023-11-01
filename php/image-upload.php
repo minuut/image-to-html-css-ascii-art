1490cb64555e0c4e4dc93570bcb7c20356d65fca
Yonnie <70378641+minuut@users.noreply.github.com>
2023-11-01 17:40:17 +0100

    Update image-upload.php

--- php/image-upload.php
+++ php/image-upload.php
@@ -1,45 +1,42 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Yonnie's Image to HTML/CSS ASCII art converter</title>
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body style="background-color: black;">
-<?php
+    <?php
 
-require_once 'ImageToAscii.php';
+    require_once 'ImageToAscii.php';
 
-try {
+    $output = "";
     if (isset($_FILES['image']) && isset($_POST['conversion_type'])) {
         $validTypes = ["image/jpeg", "image/png"];
         if (!in_array($_FILES['image']['type'], $validTypes)) {
             throw new Exception("Error: Please upload a valid image file.");
         }
-
         $imageToAscii = new ImageToAscii($_FILES['image']['tmp_name']);
 
         switch ($_POST['conversion_type']) {
             case "4px_single":
-                $imageToAscii->convertToAscii("@", 4, 50, "4px");
+                $output = $imageToAscii->convert4pxSingleCharacter();
                 break;
             case "8px_single":
-                $imageToAscii->convertToAscii("@", 8, 100, "8px");
+                $output = $imageToAscii->convert8pxSingleCharacter();
                 break;
             case "4px_multiple":
-                $imageToAscii->convertToAscii("@%#*+=-:. ", 4, 50, "4px");
+                $output = $imageToAscii->convert4pxWithMultipleCharacters();
                 break;
             case "8px_multiple":
-                $imageToAscii->convertToAscii("@%#*+=-:. ", 8, 100, "8px");
+                $output = $imageToAscii->convert8pxWithMultipleCharacters();
                 break;
         }
     }
-} catch (Exception $e) {
-    echo '<div class="error">' . $e->getMessage() . '</div>';
-}
 
-?>
-<button onclick="location.href='../index.html'">Return</button>
+    ?>
+    <button onclick="location.href='../index.html'">Return</button>
 </body>
 
 </html>
