c29ba4a9a9103477e3c3ed5fbca6a385bd967ed6
Yonnie <70378641+minuut@users.noreply.github.com>
2023-11-01 16:18:16 +0100

    Update image-upload.php

--- php/image-upload.php
+++ php/image-upload.php
@@ -10,31 +10,35 @@
 
 require_once 'ImageToAscii.php';
 
-$output = "";
-if (isset($_FILES['image']) && isset($_POST['conversion_type'])) {
-    $validTypes = ["image/jpeg", "image/png"];
-    if (!in_array($_FILES['image']['type'], $validTypes)) {
-        throw new Exception("Error: Please upload a valid image file.");
+try {
+    if (isset($_FILES['image']) && isset($_POST['conversion_type'])) {
+        $validTypes = ["image/jpeg", "image/png"];
+        if (!in_array($_FILES['image']['type'], $validTypes)) {
+            throw new Exception("Error: Please upload a valid image file.");
+        }
+        if ($_FILES['image']['size'] > 500000) {
+            throw new Exception("Error: File size is too large. Please upload a file under 500KB.");
+        }
+
+        $imageToAscii = new ImageToAscii($_FILES['image']['tmp_name']);
+
+        switch ($_POST['conversion_type']) {
+            case "4px_single":
+                $imageToAscii->convertToAscii("@", 4, 50, "4px");
+                break;
+            case "8px_single":
+                $imageToAscii->convertToAscii("@", 8, 100, "8px");
+                break;
+            case "4px_multiple":
+                $imageToAscii->convertToAscii("@%#*+=-:. ", 4, 50, "4px");
+                break;
+            case "8px_multiple":
+                $imageToAscii->convertToAscii("@%#*+=-:. ", 8, 100, "8px");
+                break;
+        }
     }
-    if ($_FILES['image']['size'] > 500000) {
-        throw new Exception("Error: File size is too large. Please upload a file under 500KB.");
-    }
-    $imageToAscii = new ImageToAscii($_FILES['image']['tmp_name']);
-    
-    switch ($_POST['conversion_type']) {
-        case "4px_single":
-            $output = $imageToAscii->convert4pxSingleCharacter();
-            break;
-        case "8px_single":
-            $output = $imageToAscii->convert8pxSingleCharacter();
-            break;
-        case "4px_multiple":
-            $output = $imageToAscii->convert4pxWithMultipleCharacters();
-            break;
-        case "8px_multiple":
-            $output = $imageToAscii->convert8pxWithMultipleCharacters();
-            break;
-    }
+} catch (Exception $e) {
+    echo '<div class="error">' . $e->getMessage() . '</div>';
 }
 
 ?>
